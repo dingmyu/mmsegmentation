8fbc84853989830243179085378953a5967970a1
ding.mingyu <ding.mingyu@bytedance.com>
2020-08-26 15:57:22 -0700

    Add resize data for cityscapes

--- resize_data.py
+++ resize_data.py
@@ -7,13 +7,13 @@ label_list = glob.glob('data/*/*/*/*/*_labelTrainIds.png')
 file_list = glob.glob('data/*/*/*/*/*_leftImg8bit.png')
 
 for index, item in enumerate(file_list):
-    img = cv2.resize(cv2.imread(item), (128, 64))
+    img = cv2.resize(cv2.imread(item), (256, 128))
     result = cv2.imwrite(item, img)
     if index % 100 == 0:
         print('img', index, img.shape)
 
 for index, item in enumerate(label_list):
-    img = cv2.resize(cv2.imread(item, flags=0), (128, 64))
+    img = cv2.resize(cv2.imread(item, flags=0), (256, 128))
     result = cv2.imwrite(item, img)
     if index % 100 == 0:
         print('label', index, img.shape)
