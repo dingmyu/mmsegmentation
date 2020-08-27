e9fd7849247393fb9b3c2a8687e9443fc2a0bb6f
ding.mingyu <ding.mingyu@bytedance.com>
2020-08-26 23:30:12 -0700

    fix bug

--- search.sh
+++ search.sh
@@ -1,4 +1,4 @@
-python3 search_small.py 9901 0 1 128 256 0 100
-#python3 search_small.py 9902 2 3 128 256 100 200
-#python3 search_small.py 9903 4 5 128 256 200 300
-#python3 search_small.py 9904 6 7 128 256 300 400
+python3 search_small.py 0 1 9901 128 256 0 100
+#python3 search_small.py 2 3 9902 128 256 100 200
+#python3 search_small.py 4 5 9903 128 256 200 300
+#python3 search_small.py 6 7 9904 128 256 300 400
