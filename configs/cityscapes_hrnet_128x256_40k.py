dd4410a94d89492370550cf1e84d4f57da73eee1
ding.mingyu <ding.mingyu@bytedance.com>
2020-08-26 20:09:07 -0700

    add resize

--- configs/cityscapes_hrnet_128x256_40k.py
+++ configs/cityscapes_hrnet_128x256_40k.py
@@ -51,11 +51,11 @@ dataset_type = 'CityscapesDataset'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (64, 128)
+crop_size = (128, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(256, 128), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(512, 256), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -68,7 +68,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(256, 128),
+        img_scale=(512, 256),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
