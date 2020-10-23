cdcd9cc052eeb5ae24cf0b29061a162d221ada80
ding.mingyu <ding.mingyu@bytedance.com>
2020-10-23 02:39:09 -0700

    Add mobilenetV2-PSP

--- mmseg/models/backbones/mobilenetv2.py
+++ mmseg/models/backbones/mobilenetv2.py
@@ -69,7 +69,9 @@ class InvertedResidual(nn.Module):
         self.with_cp = with_cp
         self.use_res_connect = self.stride == 1 and in_channels == out_channels
         hidden_dim = int(round(in_channels * expand_ratio))
-
+        self.use_transformer = True
+        if self.use_transformer and self.use_res_connect:
+            self.transformer = TransformerToken(8, in_channels)
         layers = []
         if expand_ratio != 1:
             layers.append(
@@ -105,6 +107,8 @@ class InvertedResidual(nn.Module):
 
         def _inner_forward(x):
             if self.use_res_connect:
+                if self.use_transformer:
+                    x = self.transformer(x)
                 return x + self.conv(x)
             else:
                 return self.conv(x)
